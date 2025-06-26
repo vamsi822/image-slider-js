a5e7f0f9e80f3f3b1d9247b1cf722c373080da11
Vtu26506 <vtu26506@veltech.edu.in>
2025-06-27 05:27:17 +0530

    first commit

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
-let flag= 0;
+let flag = 0;
 
 function controller(x){
     flag = flag + x;
     slideshow(flag);
-
-
 }
 
 slideshow(flag);
+
 
 function slideshow(num){
     let slides = document.getElementsByClassName('slide');
@@ -20,11 +19,13 @@ function slideshow(num){
     if(num < 0){
         flag = slides.length-1;
         num = slides.length-1;
+
     }
 
     for(let y of slides){
-        y.style.display = "none"
+        y.style.display = "none";
     }
     
-    slides[num].style.display = "block"
+    
+    slides[num].style.display = "block";
 }
